769373d0b891e72346e94223b4712b71c718a37b
SageSquash <adityacool2134@gmail.com>
2024-12-06 13:24:59 +0530

    Clean up unnecessary files and apply .gitignore

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'QApplication',
+    version= '0.0.1',
+    author=' aditya raj',
+    author_email= 'adityacool2134@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
